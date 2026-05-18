9b7a07f87dcf0324fa2658839c3a510313c91bb1
kwt <beslerkostai@gmail.com>
2026-05-18 21:29:44 +0700

    refactor: added textures

--- src/func.cpp
+++ src/func.cpp
@@ -16,9 +16,26 @@ int main()
     sf::Texture catTexture;
     if (!catTexture.loadFromFile("../images/kitty.png"))
         return -1;
-    sf::Texture objText;
-    if(!objText.loadFromFile("../images/objects.png"))
+    sf::Texture keyTexture;
+    if (!keyTexture.loadFromFile("../images/key.png"))
         return -1;
+    sf::Texture background;
+    if (!background.loadFromFile("../images/background.png"))
+        return -1;
+    sf::Texture objText1;
+    if (!objText1.loadFromFile("../images/templ1.png"))
+        return -1;
+    objText1.setRepeated(true);
+    sf::Texture objText2;
+    if (!objText2.loadFromFile("../images/templ2.png"))
+        return -1;
+    sf::Texture objText3;
+    if (!objText3.loadFromFile("../images/templ3.png"))
+        return -1;
+    sf::Texture spike;
+    if (!spike.loadFromFile("../images/spikes.png"))
+        return -1;
+    spike.setRepeated(true);
     int frameW = 180;
     int frameH = 160;
     int padding = 20;
@@ -28,7 +45,7 @@ int main()
     sf::Vector2f spawnpoint;
     spawnpoint.x = 100.f;
     spawnpoint.y = 700.f;
-
+    sf::Sprite BackGround(background);
     sf::IntRect catRect(0, 0, frameW, frameH);
     sf::Sprite cat = createCat(catTexture, frameW, frameH, WIN_WIDTH, WIN_HEIGHT);
 
@@ -39,26 +56,53 @@ int main()
     float jumpStrength = -16.0f;
     bool onGround = false;
     bool isMoving = false;
-
+    bool keyStarted = false;
+    static int keyFrameCount = 12;
+    static float keyFrameWidth = 100.f;
+    static float keySwitchTime = 0.15f;
+    static sf::Clock keyTimer;
+    static int keyFrameIdx = keyFrameCount - 1;
     std::vector<sf::Sprite> platforms;
     std::vector<sf::Sprite> spikes;
-    sf::IntRect groundRect(0, 0, 128, 16); 
-    sf::IntRect spikeRect(64, 0, 32, 32);
-
-    platforms.push_back(createObject(300.f, 600.f, objText, groundRect)); // тут можно обьектов для теста добавить
-    platforms.push_back(createObject(800.f, 600.f, objText, groundRect));
-    platforms.push_back(createObject(0.f, WIN_HEIGHT - 40.f, objText, sf::IntRect(0, 0, WIN_WIDTH, 40)));
-    spikes.push_back(createObject(500.f, WIN_HEIGHT - 70, objText, spikeRect));
+    std::vector<sf::Sprite> keys;
+    sf::IntRect groundRect(0, 0, 64, 8);
+    sf::IntRect spikeRect(0, 0, 480, 160);
+    sf::IntRect keyRect(1100, 0, 100, 200);
+    sf::Sprite key = createObject(300.0f, 450.0f, keyTexture, keyRect);
+    key.setScale(0.3f, 0.3f);
+    platforms.push_back(createObject(200.f, 550.f, objText1, groundRect)); // тут можно обьектов для теста добавить
+    platforms.back().setScale(4.0f, 4.0f);
+    platforms.push_back(createObject(800.f, 550.f, objText2, groundRect));
+    platforms.back().setScale(4.0f, 4.0f);
+    platforms.push_back(createObject(0.f, WIN_HEIGHT - 40.f, objText1, sf::IntRect(0, 0, WIN_WIDTH, 40)));
+    platforms.back().setScale(5.0f, 5.0f);
+    spikes.push_back(createObject(500.f, WIN_HEIGHT - 100, spike, spikeRect));
+    spikes.back().setScale(0.4f, 0.4f);
 
     while (win.isOpen())
     {
+        if (keyStarted == false)
+        {
+            if (keyTimer.getElapsedTime().asSeconds() > keySwitchTime)
+            {
+                keyRect.left = keyFrameIdx * keyFrameWidth;
+                key.setTextureRect(keyRect);
+                keyFrameIdx--;
+                if (keyFrameIdx < 0)
+                {
+                    keyStarted = true;
+                }
+
+                keyTimer.restart();
+            }
+        }
         sf::Event event;
         while (win.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 win.close();
         }
-        
+
         handleInput(cat, isMoving, velocityX, velocityY, onGround, jumpStrength);
         if (onGround)
         {
@@ -68,17 +112,21 @@ int main()
         handlePlatformCollisions(cat, platforms, velocityX, velocityY, onGround);
         checkSpikeCollisions(cat, spikes, spawnpoint, velocityY);
         updateAnimation(cat, catRect, isMoving, animClock, animSpeed, currentFrame, totalFrames, frameW, padding);
-        
+
         win.clear(sf::Color(50, 50, 50));
+        win.draw(BackGround);
+        win.draw(key);
         win.draw(cat);
-        for (const auto& platform : platforms) {
+        for (const auto &platform : platforms)
+        {
             win.draw(platform);
         }
-        for (const auto& spike : spikes) {
+        for (const auto &spike : spikes)
+        {
             win.draw(spike);
         }
         win.display();
     }
     return 0;
 }
-//g++ func.cpp movement.cpp objects.cpp -o f.out -lsfml-graphics -lsfml-window -lsfml-system для компиляции
+// g++ func.cpp movement.cpp objects.cpp -o f.out -lsfml-graphics -lsfml-window -lsfml-system для компиляции
--- src/objects.cpp
+++ src/objects.cpp
@@ -4,16 +4,18 @@
 #include <vector>
 #include "objects.h"
 
-sf::FloatRect getCatHitbox(const sf::Sprite& cat) {
+sf::FloatRect getCatHitbox(const sf::Sprite &cat)
+{
     sf::FloatRect bounds = cat.getGlobalBounds();
-    float width = bounds.width * 0.4f;
-    float height = bounds.height * 0.8f;
+    float width = bounds.width * 0.5f;
+    float height = bounds.height * 0.7f;
     float left = cat.getPosition().x - (width / 2.0f);
     float top = cat.getPosition().y - height;
     return sf::FloatRect(left, top, width, height);
 }
 
-sf::Sprite createObject(float x, float y, const sf::Texture &texture, sf::IntRect textureRect) {
+sf::Sprite createObject(float x, float y, const sf::Texture &texture, sf::IntRect textureRect)
+{
     sf::Sprite sprite;
     sprite.setTexture(texture);
     sprite.setTextureRect(textureRect);
@@ -21,20 +23,24 @@ sf::Sprite createObject(float x, float y, const sf::Texture &texture, sf::IntRec
     return sprite;
 }
 
-void handlePlatformCollisions(sf::Sprite& cat, const std::vector<sf::Sprite>& platforms, float dx, float& dy, bool& onGround) 
+void handlePlatformCollisions(sf::Sprite &cat, const std::vector<sf::Sprite> &platforms, float dx, float &dy, bool &onGround)
 {
     cat.move(dx, 0);
 
-    for (const auto& platform : platforms) {
+    for (const auto &platform : platforms)
+    {
         sf::FloatRect catBox = getCatHitbox(cat);
         sf::FloatRect platBox = platform.getGlobalBounds();
 
-        if (catBox.intersects(platBox)) {
-            if (dx > 0) {
+        if (catBox.intersects(platBox))
+        {
+            if (dx > 0)
+            {
                 float overlapX = (catBox.left + catBox.width) - platBox.left;
                 cat.move(-overlapX, 0);
             }
-            if (dx < 0) {
+            if (dx < 0)
+            {
                 float overlapX = (platBox.left + platBox.width) - catBox.left;
                 cat.move(overlapX, 0);
             }
@@ -42,18 +48,22 @@ void handlePlatformCollisions(sf::Sprite& cat, const std::vector<sf::Sprite>& pl
     }
     cat.move(0, dy);
     onGround = false;
-    for (const auto& platform : platforms) {
+    for (const auto &platform : platforms)
+    {
         sf::FloatRect catBox = getCatHitbox(cat);
         sf::FloatRect platBox = platform.getGlobalBounds();
 
-        if (catBox.intersects(platBox)) {
-            if (dy > 0) {
+        if (catBox.intersects(platBox))
+        {
+            if (dy > 0)
+            {
                 float overlapY = (catBox.top + catBox.height) - platBox.top;
                 cat.move(0, -overlapY);
                 dy = 0.0f;
                 onGround = true;
             }
-            if (dy < 0) {
+            if (dy < 0)
+            {
                 float overlapY = (platBox.top + platBox.height) - catBox.top;
                 cat.move(0, overlapY);
                 dy = 0.0f;
@@ -61,10 +71,12 @@ void handlePlatformCollisions(sf::Sprite& cat, const std::vector<sf::Sprite>& pl
         }
     }
 }
-void checkSpikeCollisions(sf::Sprite& cat, const std::vector<sf::Sprite>& spikes, sf::Vector2f spawnPoint, float& dy) 
+void checkSpikeCollisions(sf::Sprite &cat, const std::vector<sf::Sprite> &spikes, sf::Vector2f spawnPoint, float &dy)
 {
-    for (const auto& spike : spikes) {
-        if (getCatHitbox(cat).intersects(spike.getGlobalBounds())) {
+    for (const auto &spike : spikes)
+    {
+        if (getCatHitbox(cat).intersects(spike.getGlobalBounds()))
+        {
             cat.setPosition(spawnPoint);
             dy = 0.0f;
             break;
